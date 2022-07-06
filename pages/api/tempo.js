304baa4c36b8737482890c700b26a6754834e93d
Leonardo Sartori <leonardo.sartori@bexp.local>
2022-07-06 15:22:02 -0300

    miranha

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,8 +1,14 @@
-function Tempo(request, response) {
+async function Tempo(request, response) {
     const dynamicDate = new Date();
 
+    const spiderMan = await fetch("https://gateway.marvel.com/v1/public/characters/1009610?ts=1&apikey=fb947c0403b0f233b4fe3a9b44dc05a3&hash=37976f38a41622b0306c8ac6c7c60a41");
+    const spiderManJson = await spiderMan.json();
+    const miranha = spiderManJson.data.results.name;
+
     response.json({
-        date: dynamicDate.toGMTString()
+        date: dynamicDate.toGMTString(),
+        miranha: miranha
+
     })
 }
 
