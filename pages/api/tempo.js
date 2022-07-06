0fe5ae5737aa8a8d6a51b49210020e2429b81ef4
Leonardo Sartori <leonardo.sartori@bexp.local>
2022-07-06 15:42:02 -0300

    secret key

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,14 +1,18 @@
 async function Tempo(request, response) {
+    const apiSecret = process.env.CONVERTKIT_API_SECRET;
+    const apiSecretHash = process.env.CONVERTKIT_API_SECRET_HASH;
     const dynamicDate = new Date();
 
-    const spiderMan = await fetch("https://gateway.marvel.com/v1/public/characters/1009610?ts=1&apikey=fb947c0403b0f233b4fe3a9b44dc05a3&hash=37976f38a41622b0306c8ac6c7c60a41");
-    const spiderManJson = await spiderMan.json();
-    const miranha = spiderManJson.data.results.name;
+    // fb947c0403b0f233b4fe3a9b44dc05a3
+    // 37976f38a41622b0306c8ac6c7c60a41
+    
+    const charactersMarvel = await fetch(`https://gateway.marvel.com/v1/public/characters?ts=1&apikey=${apiSecret}&hash=${apiSecretHash}`);
+    const charactersMarvelJson = await charactersMarvel.json();
+    const characters = charactersMarvelJson.data;
 
     response.json({
         date: dynamicDate.toGMTString(),
-        miranha: miranha
-
+        characters: characters
     })
 }
 
