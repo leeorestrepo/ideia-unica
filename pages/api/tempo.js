dedb9a3a720e8cf3dadc766496904c64f0ded42e
Leonardo Sartori <leonardo.sartori@bexp.local>
2022-07-06 15:44:06 -0300

    remover secret key

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -3,9 +3,6 @@ async function Tempo(request, response) {
     const apiSecretHash = process.env.CONVERTKIT_API_SECRET_HASH;
     const dynamicDate = new Date();
 
-    // fb947c0403b0f233b4fe3a9b44dc05a3
-    // 37976f38a41622b0306c8ac6c7c60a41
-    
     const charactersMarvel = await fetch(`https://gateway.marvel.com/v1/public/characters?ts=1&apikey=${apiSecret}&hash=${apiSecretHash}`);
     const charactersMarvelJson = await charactersMarvel.json();
     const characters = charactersMarvelJson.data;
